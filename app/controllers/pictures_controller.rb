6b461acde117022c225e652234bb842b946e3370
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-10-11 17:08:52 +0900

    test_fix

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -54,11 +54,11 @@ class PicturesController < ApplicationController
   end
 
   private
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
+  def set_picture
+  @picture = Picture.find(params[:id])
+  end
 
-    def picture_params
-      params.require(:picture).permit(:image, :image_cache, :content).merge(user_id:current_user.id)
-    end
+  def picture_params
+  params.require(:picture).permit(:image, :image_cache, :content).merge(user_id:current_user.id)
+  end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
   validates :user_id, {presence: true}
+  validates :content, {presence: true, length: {maximum: 140}}
+  validates :image, {presence: true}
   mount_uploader :image, ImageUploader
 end
